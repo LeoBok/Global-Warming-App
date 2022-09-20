d88f4ecaec2d8e134aad0c89eed26499603d40e9
Leonardo Boccali <leonardoboccali19@gmail.com>
2022-09-20 12:04:17 +0200

    fixed style problem

--- src/pages/Global Warming News/SingleNews.js
+++ src/pages/Global Warming News/SingleNews.js
@@ -1,5 +1,5 @@
 import './GbNews.css'
-import { useParams } from "react-router-dom"
+import { Link, useParams } from "react-router-dom"
 import {carouselData} from "../../components/CarouselComp/carouselData";
 
 const SingleNews = () => {
@@ -7,16 +7,20 @@ const SingleNews = () => {
   const singleNotice = carouselData.find(data => data.id == singleNewsId)
 
   return (
-    <div className='p-4 py-md-4 mx-auto'>
-      <article className="singleNews p-4 py-md-3 mx-auto">
+    <div className='p-4 p-lg-0 mx-auto'>
+      <article className="singleNews p-4 py-md-3 p-lg-3 mt-lg-3 mx-auto">
           <img
             src={singleNotice.imgSource}
             className='card-img-top news-img'
             alt={singleNotice.title}
           />
-          <h4 className="p-3">{singleNotice.title}</h4>
+          <h4 className="py-3">{singleNotice.title}</h4>
           <p>{singleNotice.extendedParagraph}</p>
+          
+          <Link to='/'>Back to Home</Link>
       </article>
+
+      
   </div>
   )
 }
--- src/pages/TemperaturePage/Temperature-anomalies.js
+++ src/pages/TemperaturePage/Temperature-anomalies.js
@@ -12,7 +12,8 @@ const TemperatureAnomalies = () => {
                 <TempComp tempData={tempData?.result} isLoading={isLoading} />
                 <Description
                         description={
-                        <>
+                        <>  
+                            The current global warming rate is not natural. From 1880 to 1981 was (0.07°C / 0.13°F) per decade. Since 1981 this rate has increased to (0.18°C / 0.32°F).
                             Since the industrial revolution, the average increase of global temperature is <span className="highlight-text rounded-1 p-1">about
                             1,0° C (1,8° F)</span>.
                             The northern hemisphere of the Earth is warming faster. The Arctic warmed <span className="highlight-text rounded-1 p-1">between 2° C (3.6° F) and 4° C (7.2° F)</span>.
